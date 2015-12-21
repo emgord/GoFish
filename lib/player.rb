05e43b576e472dd9f4c5cb1372b14b629ff734bd
Tammy <misstamar@gmail.com>
2015-12-21 14:19:10 -0800

    initialized player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,14 @@
 module GoFish
   class Player
+    attr_accessor :name, :secret_hand, :table_hand
+
+    def initialize(name)
+      @name = name
+      @secret_hand = Hand.new.drow(@deck, 5)
+      @table_hand = Hand.new
+    end
+
+
   end
 
 end
