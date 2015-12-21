65bfffcef16e49181d510b45fb5eec013dcf437e
Charles Ellis <hamled@hamled.name>
2015-12-21 15:05:27 -0800

    Player spec: More attribute tests

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe Player do
     it "has a name" do
       expect(subject.name).to be_a String
     end
+    it "has a secret hand" do
+      expect(subject.secret_hand).to be_a RubyCards::Hand
+    end
+    it "has a table (displayed) hand" do
+      expect(subject.table_hand).to be_a RubyCards::Hand
+    end
   end
 
   describe "#initialize" do
