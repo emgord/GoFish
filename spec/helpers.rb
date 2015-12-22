f886514b52b86d22030adea04ca609f18111448c
Charles Ellis <hamled@hamled.name>
2015-12-21 16:33:54 -0800

    Specs: Additional helper method

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -1,4 +1,8 @@
 module Helpers
+  def find_card_in_hand(player)
+    return player.secret_hand.cards[0]
+  end
+
   def find_card_not_in_hand(hand)
     card = RubyCards::Card.new(2, 'Club')
     while hand.cards.include? card
